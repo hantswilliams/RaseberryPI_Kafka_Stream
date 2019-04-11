6e2083a80fff9c8647d7c93768f54db3ae6a293c
Hants Williams <hantsawilliams@gmail.com>
2019-04-10 23:31:38 -0400

    ....

--- startkafka.sh
+++ startkafka.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd 
 cd /opt/Kafka/kafka_2.12-2.2.0/
-sudo bin/kafka-server-start.sh config/server.properties & echo $! kafka_pid.txt
+sudo bin/kafka-server-start.sh config/server.properties & echo $! /opt/Kafka/kafka_2.12-2.2.0/kafka_pid.txt
