6f3765ac273fef44267d4ef59f44e1a75066d455
Hants Williams <hantsawilliams@gmail.com>
2019-04-12 22:34:33 -0400

    added landing

--- local_version_raspberrypi/consumer/app.py
+++ local_version_raspberrypi/consumer/app.py
@@ -18,31 +18,29 @@ consumer = KafkaConsumer(
 app = Flask(__name__)
 
 
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('home.html')
+
 
 @app.route('/dogcam', methods=['GET'])
 def dogcam():
-    return render_template('landing.html')
+    return render_template('camera.html')
 
 
 @app.route('/video', methods=['GET'])
 def video():
-    """
-    This is the heart of our video display. Notice we set the mimetype to 
-    multipart/x-mixed-replace. This tells Flask to replace any old images with 
-    new values streaming through the pipeline.
-    """
     return Response(
         get_video_stream(), 
         mimetype='multipart/x-mixed-replace; boundary=frame')
 
 def get_video_stream():
-    """
-    Here is where we recieve streamed images from the Kafka Server and convert 
-    them to a Flask-readable format.
-    """
     for msg in consumer:
         yield (b'--frame\r\n'
                b'Content-Type: image/jpg\r\n\r\n' + msg.value + b'\r\n\r\n')
+
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
