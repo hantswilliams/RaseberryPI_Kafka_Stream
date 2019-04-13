7847f4c362abc489fb77047264ba97d5a64b8ae0
Hants Williams <hantsawilliams@gmail.com>
2019-04-12 22:12:53 -0400

    updated

--- local_version_raspberrypi/consumer/app.py
+++ local_version_raspberrypi/consumer/app.py
@@ -1,5 +1,5 @@
 import datetime
-from flask import Flask, Response
+from flask import Flask, Response, render_template, request, redirect, url_for
 from kafka import KafkaConsumer
 
 
@@ -16,6 +16,13 @@ consumer = KafkaConsumer(
 
 # Set the consumer in a Flask App
 app = Flask(__name__)
+
+
+
+@app.route('/dogcam', methods=['GET'])
+def dogcam():
+    return render_template('landing.html')
+
 
 @app.route('/video', methods=['GET'])
 def video():
