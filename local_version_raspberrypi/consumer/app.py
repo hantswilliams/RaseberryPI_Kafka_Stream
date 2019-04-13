89ee44a04d18d56697288113ed96e9c8125c1685
Hants Williams <hantsawilliams@gmail.com>
2019-04-12 23:12:43 -0400

    colors

--- local_version_raspberrypi/consumer/app.py
+++ local_version_raspberrypi/consumer/app.py
@@ -41,9 +41,17 @@ def do_admin_login():
 
 @app.route('/dogcam', methods=['GET'])
 def dogcam():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
     return render_template('camera.html')
 
 
+
+@app.route("/logout")
+def logout():
+session['logged_in'] = False
+return home()
 
 
 
